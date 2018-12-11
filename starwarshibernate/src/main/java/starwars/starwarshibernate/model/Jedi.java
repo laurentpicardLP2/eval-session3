0338196c1d3cb9b96cef76e293a37d78e574152e
CedricT33 <cedric.tressous@gmail.com>
2018-12-11 21:31:58 +0100

    Ajout du model Jedi.java

--- starwarshibernate/src/main/java/starwars/starwarshibernate/model/Jedi.java
+++ starwarshibernate/src/main/java/starwars/starwarshibernate/model/Jedi.java
@@ -70,6 +70,49 @@ public class Jedi implements Serializable{
 	public void setArmes(Set<Arme> armes) {
 		this.armes = armes;
 	}
+
+	@Override
+	public String toString() {
+		return "Jedi [id=" + id + ", name=" + name + ", firstname=" + firstname + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Jedi other = (Jedi) obj;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
 	
 	
 }
